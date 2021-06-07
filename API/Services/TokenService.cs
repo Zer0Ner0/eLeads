466e8343a7d0b0714ff78ac54cd57a81c9e6ef64
Amir Mazlan <amirmz48@gmail.com>
2021-06-08 00:57:39 +0800

    Adding a token service

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,13 @@
+using API.Controllers.Interfaces;
+using API.Entities;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
