93586679c81d69a2ca13cd3d9bfa39385636eb32
Amir Mazlan <amirmz48@gmail.com>
2021-06-17 22:56:48 +0800

    Creating the member interface

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,23 @@
+import { Photo } from "./photo";
+
+declare module namespace {
+
+    export interface Member {
+        id: number;
+        username: string;
+        photoUrl: string;
+        age: number;
+        knownAs: string;
+        created: Date;
+        lastActive: Date;
+        gender: string;
+        introduction: string;
+        lookingFor: string;
+        interests: string;
+        city: string;
+        country: string;
+        photos: Photo[];
+    }
+    
+}
+
